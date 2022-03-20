8eed38e841741053a6d82ae239fdb452e4c78f02
Sylvia <josephao424@gmail.com>
2022-03-20 17:51:00 -0400

    Fix small error

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='LoL-Scrapper',
+    name='LoL_Scrapper',
     version='0.1',
     description='Tool For getting Data off of LoL websites',
     author='AlsoSylv',
